2756c0febb7db0e565d06a80ee677c70172adbf3
Kota Mizushima <mizukota@gmail.com>
2017-05-18 18:15:59 +0900

    Organize as Python module

--- README.md
+++
@@ -1,3 +0,0 @@
-# py_combinator
-
-A parser combinator library written in Python to learn Python myself
--- combinator/parser.py
+++ combinator/parser.py
@@ -1,28 +1,9 @@
+# -*- coding: utf-8 -*-
+
 import re
 
-class ParseResult:
-    def __init__(self, next):
-        self.next = next
-
-    def success(self):
-        pass
-
-class ParseSuccess(ParseResult):
-    def __init__(self, value, next):
-        super(ParseSuccess, self).__init__(next)
-        self.value = value
-
-    @property
-    def success(self):
-        return True
-
-class ParseFailure(ParseResult):
-    def __init__(self, next):
-        super(ParseFailure, self).__init__(next)
-
-    @property
-    def success(self):
-        return False
+from combinator import parse_result
+from parse_result import *
 
 class Parser:
     def parse(self, input):
@@ -60,7 +41,7 @@ class Parser:
     def chainl(self, q):
         return self.chainlI(self, q)
 
-class RepeatParser(Parser):
+class RepeatParser(Parser): 
     def __init__(self, parser):
         super(RepeatParser, self).__init__()
         self.parser = parser
@@ -177,34 +158,140 @@ def rule(fun):
 def chainl(p, q):
     return p.chainl(q)
 
-E = rule(lambda : A)
-A = rule((lambda :
-        M.chainl(
-            (
-                s('+').map(lambda op: (lambda lhs, rhs: lhs + rhs))
-            ).mor(
-                s('-').map(lambda op: (lambda lhs, rhs: lhs - rhs))
-            )
-        )))
-M = rule((lambda :
-        P.chainl(
-            (
-                s('*').map(lambda op: (lambda lhs, rhs: lhs * rhs))
-            ).mor(
-                s('/').map(lambda op: (lambda lhs, rhs: lhs / rhs))
-            )
-        )))
-P = rule((lambda :
-        (s('(').cat(E)).cat(s(')')).map(lambda values: values[0][1]).mor(N)
-        ))
+class Parser:
+    def parse(self, input):
+        return ParseFailure(input)
 
-N = rule((lambda :
-        reg('[0-9]+').map(lambda n: int(n))))
+    def mor(self, rhs):
+        return OrParser(self,  rhs)
+
+    def cat(self, rhs):
+        return CatParser(self, rhs)
+
+    def map(self, fun):
+        return MapParser(self, fun)
+
+    def flat_map(self, fun):
+        return FlatMapParser(self, fun)
+
+    def rep(self):
+        return RepeatParser(self)
+
+    def chainlI(self, p, q):
+        def fun(value):
+            x = value[0]
+            xs = value[1]
+            a = x
+            while len(xs)> 0:
+                f = xs[0][0]
+                b = xs[0][1]
+                a = f(a, b)
+                del xs[0]
+            return a
+        value = self.cat((q.cat(p).rep())).map(fun)
+        return value
+
+    def chainl(self, q):
+        return self.chainlI(self, q)
+
+class RepeatParser(Parser): 
+    def __init__(self, parser):
+        super(RepeatParser, self).__init__()
+        self.parser = parser
+
+    def parse(self, input):
+        rest = input
+        values = []
+        while True:
+            r = self.parser.parse(rest)
+            if not r.success: return ParseSuccess(values, rest)
+            values.append(r.value)
+            rest = r.next
+        pass
 
 
-r = int(E.parse("111+222").value)
-print(r)
-r = int(E.parse("(1+2*3)*4").value)
-print(r)
-r = int(E.parse("(3+2*4)/3").value)
-print(r)
+class RegexParser(Parser):
+    def __init__(self, regex_string):
+        super(RegexParser, self).__init__()
+        self.pattern = re.compile(regex_string)
+
+    def parse(self, input):
+        m = self.pattern.match(input)
+        if m != None:
+            s, e = m.span()
+            return ParseSuccess(input[s:e], input[e:])
+        else:
+            return ParseFailure(input)
+
+class FlatMapParser(Parser):
+    def __init__(self, parser, fun):
+        super(FlatMapParser, self).__init__()
+        self.parser = parser
+        self.fun = fun
+
+    def parse(self, input):
+        r = self.parser.parse(input)
+        if not r.success: return r
+        return self.fun(r.value).parse(r.next)
+
+class MapParser(Parser):
+    def __init__(self, parser, fun):
+        super(MapParser, self).__init__()
+        self.parser = parser
+        self.fun = fun
+
+    def parse(self, input):
+        r = self.parser.parse(input)
+        if not r.success: return r
+        return ParseSuccess(self.fun(r.value), r.next)
+
+class OrParser(Parser):
+    def __init__(self, lhs, rhs):
+        super(OrParser, self).__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def parse(self, input):
+        r = self.lhs.parse(input)
+        if r.success:
+            return r
+        else:
+            return self.rhs.parse(input)
+
+
+class CatParser(Parser):
+    def __init__(self, lhs, rhs):
+        super(CatParser, self).__init__()
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def parse(self, input):
+        r1 = self.lhs.parse(input)
+        if r1.success:
+            r2 = self.rhs.parse(r1.next)
+            if r2.success:
+                return ParseSuccess([r1.value, r2.value], r2.next)
+            else:
+                return ParseFailure(r1.next)
+        else:
+            return ParseFailure(input)
+
+class StringParser(Parser):
+    def __init__(self, literal):
+        super(StringParser, self).__init__()
+        self.literal = literal
+
+    def parse(self, input):
+        if input.startswith(self.literal):
+            rest = input[len(self.literal):]
+            return ParseSuccess(self.literal, rest)
+        else:
+            return ParseFailure(input)
+
+class DelayedParser(Parser):
+    def __init__(self, fun):
+        super(DelayedParser, self).__init__()
+        self.fun = fun
+
+    def parse(self, input):
+        return self.fun().parse(input)
